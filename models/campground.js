85a6fc5e3234a041f0d2db03f46232ae2a0e3782
himsin98 <himsin98@gmail.com>
2022-02-25 19:00:37 +0530

    Added rating and reviews

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema; //is we don't do this we will have to refer it always as mongoose.Schema
 
 const CampgroundSchema = new Schema({
@@ -6,7 +7,17 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({ _id: { $in: doc.reviews } })
+    }
 })
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
